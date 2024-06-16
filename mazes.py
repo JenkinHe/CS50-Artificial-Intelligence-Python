0dbdaf0a75331b3dc20797572ab6d1e669509adf
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-06-16 14:59:19 +1000

    maze class

--- mazes.py
+++ mazes.py
@@ -36,3 +36,46 @@ class QueueFrontier(StackFrontier):
             node =self.frontier[0]
             self.frontier = self.frontier[1:]
             return node
+        
+
+class Maze():
+
+    def __init__(self,filename):
+
+        #Read file and set height and width of maze 
+        with open(filename) as f:
+            contents=f.read()
+        
+        # Validate start and goal
+        if contents.count("A")!=1:
+            raise Exception("maze must have exactly one start point")
+        if contents.count("B")!=1:
+            raise Exception("maze must have exactly one goal ")
+        
+        # Determine height and width of maze
+        contents = contents.splitlines()
+        self.height=len(contents)
+        self.width = max(len(line) for line in contents)
+
+        # Keep track of walls
+        self.walls =[]
+        for i in range(self.height):
+            row=[]
+            for j in range(self.width ):
+                try:
+                    if contents[i][j]=="A":
+                        self.start=(i,j)
+                        row.append(False)
+                    elif contents[i][j]=="B":
+                        self.goal=(i,j)
+                        row.append(False)
+                    elif contents[i][j]==" ":
+                        row.append(False)
+                    else:
+                        row.append(True)
+                except IndexError:
+                    row.append(False)
+            self.walls.append(row)
+        
+        self.solution=None
+        
