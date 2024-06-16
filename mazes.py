bd6629616045f58ffd2f117e153512ccab912b67
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-06-16 14:49:21 +1000

    queue frontier inherits stack frontier but ovverides the remove method

--- mazes.py
+++ mazes.py
@@ -26,3 +26,13 @@ class StackFrontier():
             node= self.frontier[-1]
             self.frontier = self.frontier[:-1]
             return node
+        
+class QueueFrontier(StackFrontier):
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node =self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
